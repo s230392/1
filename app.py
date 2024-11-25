3d17cd267e4f96f5fa60988b298f600859fb990a
s230392 <gimpo.s230392@ggh.goe.go.kr>
2024-11-25 14:34:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,16 +17,19 @@ with col1:
       st.image('시각화 파일1.png' ) 
 with col2:
       st.subheader('데이터시각화2')
-      st.image('____________')
+      st.image('시각화 파일2.png')
 with col3:
       st.subheader('데이터시각화3')
-      st.image('____________')
+      st.image('시각화 파일3.png')
 with col4:
       st.subheader('데이터시각화4')
-      st.image('____________')
+      st.image('시각화 파일4.png')
 with col5:
       st.subheader('데이터시각화5')
-      st.image('____________')
+      st.image('시각화 파일5.png')
+ with col6:
+      st.subheader('데이터시각화5')
+      st.image('시각화 파일6.png')
 
 a = st.number_input(' 오늘의 수면 시간을 입력해주세요 ', value=0) 
 b = st.number_input(' 오늘의 공부 시간을 입력해주세요 ', value=0) 
@@ -34,7 +37,7 @@ c = st.number_input(' 오늘 인터넷 사용 시간을 입력해주세요 ', va
 d = st.number_input(' 오늘 마신 카페인 양을 입력해주세요 ', value=0) 
 e = st.number_input(' 오늘 운동 시간을 입력해주세요 ', value=0) 
 if st.button('수면의질 예측'):             
-        input_data = [[ a,b,c,d,e,f ]]          
+        input_data = [[ a,b,c,d,e ]]          
         p = model.predict(input_data)      
         st.write('인공지능이 예측한 당신의 수면의 질은', p)
 
